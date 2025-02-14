b857eeb3164641ffb212147d18af2ee812f0edd8
giusmili <giusmili67@gmail.com>
2025-02-14 14:43:48 +0100

    js

--- src/app.js
+++ src/app.js
@@ -17,13 +17,35 @@ document.addEventListener("DOMContentLoaded", (e) => {
                                     <li><strong>genre</strong> : ${element.genre}</li>
                                     <li><strong>sortie dvd</strong> : ${element.sortie}</li>
                                 </ul>
+                                <button 
+                                type="button" 
+                                aria-label="link" 
+                                class="btn-primary">Consulter le site officiel
+                                </button>
                                 </figcaption>
                                 </figure>
                                 `
 
 })
+const btns = document.querySelectorAll(".btn-primary");
 
-  console.table(collection);
+        const links = [
+            "https://www.allocine.fr/film/fichefilm_gen_cfilm=27782.html",
+            "https://www.allocine.fr/film/fichefilm_gen_cfilm=296.html",
+            "https://www.allocine.fr/film/fichefilm_gen_cfilm=727.html"
+        ];
+
+        btns.forEach((button, index) => {
+            console.log(button);
+
+            button.addEventListener("click", () => {
+                if (index < links.length) {
+                    window.open(links[index]);
+                } else {
+                    window.alert("No link associated with this button");
+                }
+            });
+        });
 
   /* localStorage */
   localStorage.setItem("collection", JSON.stringify(collection));
